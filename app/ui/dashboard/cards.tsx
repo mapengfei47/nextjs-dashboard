3e2d8b6b9da01e63cf9516c598e52241f7e6aa5e
allanma <allanma@xiaoe-tech.com>
2025-11-28 23:34:51 +0800

    feat: learn streaming and sarch pagination

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,8 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
+
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -13,17 +15,13 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper({
-  totalPaidInvoices,
-  totalPendingInvoices,
-  numberOfInvoices,
-  numberOfCustomers,
-}: {
-  totalPaidInvoices: number | string,
-  totalPendingInvoices: number | string,
-  numberOfInvoices: number | string,
-  numberOfCustomers: number | string
-}) {
+export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
